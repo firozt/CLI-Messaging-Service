70d27a478ef154beaac8bfdbab027c55a0f81f93
firozt <firozt03@gmail.com>
2025-07-23 12:04:46 +0100

    tidbits

---
+++ src/net/SignalingServer.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Ramiz Abdulla on 15/07/2025.
+//
+
+
+
+/*
+ * OBJECTIVES:
+ * - UDP holepunch through peers NAT to allow connection
+ * HOW?:
+ * - Confirm IP/Port to each other
+ * - On confirmation send each others IP/Port to both peers
+*/
+#include "SignalingServer.h"
---
+++ src/net/SignalingServer.h
@@ -0,0 +1,16 @@
+//
+// Created by Ramiz Abdulla on 15/07/2025.
+//
+
+#ifndef SIGNALINGSERVER_H
+#define SIGNALINGSERVER_H
+#include "BaseServer.h"
+
+
+class SignalingServer: public BaseServer{
+    SignalingServer();
+};
+
+
+
+#endif //SIGNALINGSERVER_H
